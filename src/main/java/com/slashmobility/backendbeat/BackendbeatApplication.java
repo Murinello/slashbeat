6d1f1e45671a1204fcb95c95fd25a29c67afa611
murinello <pedro.pm.vale@gmail.com>
2020-09-09 12:10:03 +0200

    first commit

---
+++ src/main/java/com/slashmobility/backendbeat/BackendbeatApplication.java
@@ -0,0 +1,13 @@
+package com.slashmobility.backendbeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendbeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendbeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slashmobility/backendbeat/BackendbeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slashmobility.backendbeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
